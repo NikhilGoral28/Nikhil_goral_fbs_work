f398b49487bcc55e40b427dd940803681dbcf1b6
NikhilGoral28 <nikhilgoral587@gmail.com>
2025-12-22 19:21:45 +0530

    commit

---
+++ Core_python/Assignments/Assignment_3/q1.py
@@ -0,0 +1,11 @@
+#Write a program to check if the given number is positive or negative
+
+
+
+num = int(input("Enter a number: "))
+
+
+if num > 0:
+    print(f"Number {num} is positive")
+else:
+    print(f"Number {num} is negative")
