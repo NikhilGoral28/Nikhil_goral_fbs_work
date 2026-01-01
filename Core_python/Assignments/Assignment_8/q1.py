14e97cd2162366db2630de0290f1749356780cd3
NikhilGoral28 <nikhilgoral587@gmail.com>
2026-01-01 10:50:48 +0530

    A8 - q1

---
+++ Core_python/Assignments/Assignment_8/q1.py
@@ -0,0 +1,13 @@
+#Write a program to calculate area of rectangle 
+
+def area():
+    len = int(input("Enter length of rectangle: "))
+    bre = int(input("Enter breadth of rectangle: "))
+
+    area = len * bre
+
+    print(f"Area of Rectangle is {area}")
+
+
+
+area()
