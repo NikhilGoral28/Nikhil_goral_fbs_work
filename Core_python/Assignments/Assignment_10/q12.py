fe82842df2f96aa3878d3349fef85491525d0668
NikhilGoral28 <nikhilgoral587@gmail.com>
2026-01-12 17:17:27 +0530

    q12

--- Core_python/Assignments/Assignment_10/q12.py
+++ Core_python/Assignments/Assignment_10/q12.py
@@ -1,1 +1,13 @@
-#
+#12 . Write a program to create three lists of numbers, their squares and cubes 
+
+
+
+lst = list(map(int,input("Enter a number: ").split()))
+ 
+square = [i**2 for i in lst]
+
+cube = [i**3 for i in lst]
+
+print(lst)
+print(square)
+print(cube)
