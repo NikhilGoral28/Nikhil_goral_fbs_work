ade00768ae3a8e10c1f2e54acdbea6c8c9c2656c
NikhilGoral28 <nikhilgoral587@gmail.com>
2026-01-19 00:19:33 +0530

    A11 - q7

--- Core_python/Assignments/Assignment_11/q7.py
+++ Core_python/Assignments/Assignment_11/q7.py
@@ -4,3 +4,14 @@
 lst1 = [10,20,30,40,50]
 
 lst2 = [40,50,60,70,80,90]
+
+
+intersection_lst = []
+
+for item in lst1:
+
+    if item in lst2:
+        intersection_lst.append(item)
+
+
+print("Intersection of two list: ",  intersection_lst)
